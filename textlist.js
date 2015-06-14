1215961c54aaa6320ca9c3a6e37d0c7a7a1d09e3
Clarissa Littler <clarissa.littler@gmail.com>
2015-06-13 19:35:50 -0700

    finished basic collections examples, but need to do a second pass of explanatory text and write out exercises

---
+++ textlist.js
@@ -0,0 +1,66 @@
+
+$(document).ready( function () {
+
+var TextModel = Backbone.Model.extend({
+    defaults : {"value" : ""}
+});
+
+TextModel.prototype.replace = function (str) {
+    this.set("value", str);
+};
+
+var TextView = Backbone.View.extend({
+    render: function () {
+        var textVal = this.model.get("value");
+        var btn = '<button>Edit</button>';
+        var input = '<input type="text" value="' + textVal + '" />';
+        this.$el.html("<div>" + input + btn + "</div>");
+    },
+    initialize: function () {
+        this.model.on("change", this.render, this);
+    },
+    events : {
+        "click button" : "replace"
+    },
+    replace : function () {
+        var str = this.$el.find("input").val();
+        this.model.replace(str);
+    }
+});
+
+var TextCollection = Backbone.Collection.extend({
+    model : TextModel
+});
+
+var TextCollectionView = Backbone.View.extend({
+    render : function () {
+        var btn = '<button id="addbutton">Add Text</button>';
+        var div = '<div id="text-list"></div>';
+        this.$el.html(div + btn);
+    },
+    initialize : function () {
+        this.listenTo(this.collection, 'add', this.addOne);
+    },
+    events : {
+        "click #addbutton" : "addCollection"
+    },
+    addOne : function (txt) {
+        txt.set("value","Enter something here...");
+        var view = new TextView({model : txt});
+        view.render();
+        this.$("#text-list").append(view.$el);
+    },
+    addCollection : function () {
+        this.collection.create();
+    }
+});
+
+var textCollection = new TextCollection();
+
+var textCollectionView = new TextCollectionView({ collection : textCollection});
+
+textCollectionView.render();
+
+$("#listdiv").append(textCollectionView.$el);
+
+});
