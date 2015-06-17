aae79ebc696d0813c463a3e4af8bd169f247a7c9
Clarissa Littler <clarissa.littler@gmail.com>
2015-06-17 10:23:18 -0700

    have basic collections-with-server example done, but still need explanatory text and text for exercises

---
+++ textlistServe.js
@@ -0,0 +1,85 @@
+
+$(document).ready( function () {
+
+var idCount = 0;
+
+var TextModel = Backbone.Model.extend({
+    defaults : {"value" : ""},
+    initialize : function () {
+        this.fetch();
+    },    
+    replace : function (str) {
+        this.set("value", str);
+        this.save();
+    }
+});
+
+var TextView = Backbone.View.extend({
+    render: function () {
+        var textVal = this.model.get("value");
+        var btn = '<button>Clear</button>';
+        var input = '<input type="text" value="' + textVal + '" />';
+        this.$el.html("<div>" + input + btn + "</div>");
+    },
+    initialize: function () {
+        this.model.on("change", this.render, this);
+    },
+    events : {
+        "click button" : "clear",
+        "keypress input" : "updateOnEnter"
+    },
+    replace : function () {
+        var str = this.$el.find("input").val();
+        this.model.replace(str);
+    },
+    clear: function () {
+        this.model.replace("");
+    },
+    updateOnEnter: function (e){
+        if(e.keyCode == 13) {
+            this.replace();
+        }
+    }
+});
+
+var TextCollection = Backbone.Collection.extend({
+    model : TextModel,
+    url : "/texts",
+    initialize: function () {
+        this.fetch();
+    }
+});
+
+var TextCollectionView = Backbone.View.extend({
+    render : function () {
+        var btn = '<button id="addbutton">Add Text</button>';
+        var div = '<div id="text-list"></div>';
+        this.$el.html(div + btn);
+    },
+    initialize : function () {
+        this.listenTo(this.collection, 'add', this.addOne);
+    },
+    events : {
+        "click #addbutton" : "addCollection"
+    },
+    addOne : function (txt) {
+        txt.set("value","Enter something here...");
+        var view = new TextView({model : txt});
+        view.render();
+        this.$("#text-list").append(view.$el);
+    },
+    addCollection : function () {
+        this.collection.create({id : idCount});
+        idCount = idCount+1;
+    }
+});
+
+var textCollection = new TextCollection();
+
+var textCollectionView = new TextCollectionView({ collection : textCollection});
+
+textCollectionView.render();
+
+$("#listdiv").append(textCollectionView.$el);
+
+});
